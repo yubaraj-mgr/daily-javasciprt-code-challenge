3d8b2291fcc1caee73999a6d165f3afab59947bc
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-06-01 07:48:08 +1000

    hint are added

--- script.js
+++ script.js
@@ -42,7 +42,8 @@ Note: for this kata y isn't considered a vowel.
 
 // Answer
 function disemvowel(str) {
-  return str.replace(/[aeiou]/gi, "");
+  return str.replace(/[aeiou]/gi, ""); // g means global which checks whole string
+  //   not just the first char of string and i means case sensitive
 }
 
 /*
@@ -60,7 +61,7 @@ function spinWords(string) {
     if (item.length < 5) {
       finalArray.push(item);
     } else {
-      finalArray.push(item.split("").reverse().join(""));
+      finalArray.push(item.split("").reverse().join("")); //reverse only works on array not on string
     }
   });
   return finalArray.join(" ");
@@ -81,7 +82,9 @@ Examples
 //Answer
 function digital_root(n) {
   // ...
-  const array = Array.from(String(n), Number);
+  const array = Array.from(String(n), Number); //String(n) means convert n to string because number cannot
+  //directyly convert into array so we have to convert it into String first
+  // , Number means it will change the individual array into Numbers instead of String.
   const sum = array.reduce((a, b) => a + b);
   if (sum >= 10) {
     return digital_root(sum);
@@ -129,4 +132,40 @@ function highAndLow(numbers) {
   const max = Math.max(...numberArray);
   const min = Math.min(...numberArray);
   return `${max} ${min}`;
+}
+
+/*
+8.
+You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this" 
+*/
+
+//Answer
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return names[0] + " likes this";
+    case 2:
+      return names[0] + " and " + names[1] + " like this";
+    case 3:
+      return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+    default:
+      return (
+        names[0] +
+        ", " +
+        names[1] +
+        " and " +
+        (names.length - 2) +
+        " others like this"
+      );
+  }
 }
