eb1d23ea24e2f7abf4ced550316ec2821b2b78f1
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-06-04 12:22:16 +1000

    this is my second challenge today

--- script.js
+++ script.js
@@ -211,3 +211,46 @@ var countBits = function (n) {
   });
   return sum;
 };
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+Accepted
+1,615,925
+Submissions
+4,078,414
+*/
+var longestCommonPrefix = function (strs) {
+  let i = 0;
+  while (i < strs[0].length) {
+    let char = strs[0][i];
+    for (let s = 1; s < strs.length; s++) {
+      if (strs[s][i] != char) {
+        return strs[s].slice(0, i);
+      }
+    }
+    i++;
+  }
+
+  return strs[0];
+};
