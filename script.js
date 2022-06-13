5c74a732ca0b08d9b9b581a1d16b1723d47df738
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-06-13 13:28:20 +1000

    Today's challenge

--- script.js
+++ script.js
@@ -366,34 +366,36 @@ var numberOfSteps = function (num) {
 };
 
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
+Given a string s, find the length of the longest substring without repeating characters.
 
  
 
 Example 1:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 Example 2:
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
 Example 3:
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
- 
-
-Constraints:
-
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 */
+
+function lengthOfLongestSubstring(s) {
+  const map = {};
+  var left = 0;
+
+  return s.split("").reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0);
+}
