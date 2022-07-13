efa57a5990a4488a04daf03a2dea8ffd31636592
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-07-13 13:30:31 +1000

    another day another challenge

--- script.js
+++ script.js
@@ -458,3 +458,50 @@ var addTwoNumbers = function (l1, l2) {
 
   return List.next;
 };
+
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false*/
+
+var isValid = function (s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    switch (c) {
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      default:
+        if (c !== stack.pop()) {
+          return false;
+        }
+    }
+  }
+
+  return stack.length === 0;
+};
