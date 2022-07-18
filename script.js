f7e8f246fb2fdcf1166d4896cc7c514da4458710
Yubaraj Magar <yubaraj.100mgr@gmail.com>
2022-07-18 10:26:35 +1000

    today

--- script.js
+++ script.js
@@ -505,3 +505,56 @@ var isValid = function (s) {
 
   return stack.length === 0;
 };
+
+/*
+  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+var isValid = function (s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    switch (c) {
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+      case "{":
+        stack.push("}");
+        break;
+      default:
+        if (c !== stack.pop()) {
+          return false;
+        }
+    }
+  }
+
+  return stack.length === 0;
+};
